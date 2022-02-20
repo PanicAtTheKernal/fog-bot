cc14818b12c6b8e98f1a78396464f075e158d7fb
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2022-02-20 19:31:55 +0000

    Started on work on the YamlValidator class -Added all the properties to the class -Added a constructor to the class -Created the function valid_yaml, which will validate if the yaml is valid -Created the function valid_schema, which will make sure that the yaml follows the schema -Created the function valid_issues in the GitlabIssues class which will validate all the issues retrieved by the object -Created the function print_comment in the GitlabIssues which will print a comment under an issue

--- gitlab-bot.py
+++ gitlab-bot.py
@@ -40,9 +40,9 @@ class GitlabHandler:
     def get_client(self):
         return self._client
 
-    def create_issue_handler(self, labels: [str], state: [str], order_by: str, sort: str):
-        if self._client != None:
-
+    def create_issue_handler(self, labels: [str], state='opened', order_by='created_at', sort='desc'):
+        if self._client is not None:
+            return GitlabIssues(self._client, self._project_id, labels, state, order_by, sort)
 
 
 class GitlabIssues:
@@ -53,21 +53,34 @@ class GitlabIssues:
     __issues = []
     __project_id = ""
     __project = None
+    __client = None
 
-    def __init__(self, project_id, labels: [str], state='opened', order_by='created_at', sort='desc'):
+    def __init__(self, client, project_id, labels: [str], state='opened', order_by='created_at', sort='desc'):
         self.__labels = labels
         self.__state = state
         self.__order_by = order_by
         self.__sort = sort
         self.__project_id = project_id
+        self.__client = client
 
-    def request_issues(self, client):
-        if self.__project is None:
-            self.__project = client.project.get(id=self.__project_id)
+    def request_issues(self):
+        if self.__project is not None:
+            self.__project = self.__client.project.get(id=self.__project_id)
         self.__issues = self.__project.issues.list(labels=self.__labels,
                                                    state=self.__state,
                                                    order_by=self.__order_by,
                                                    sort=self.__sort)
+
+    def validate_issues(self):
+        for issue in self.__issues:
+            YamlValidator(issue).validate_yaml(self)
+
+    def print_comment(self, issue, message, labels):
+        issue_t = self.__project.issues.get(issue.attributes['iid'])
+        issue_t.labels = [labels]
+        issue_t.notes.create({'body': message})
+        issue_t.save()
+        print("The issue has been updated")
 
     def get_issues(self):
         return self.__issues
@@ -78,29 +91,13 @@ class GitlabIssues:
     def get_labels(self):
         return self.__labels
 
+
 class GitlabMergeRequest:
     pass
 
 
 class YamlValidator:
-    pass
-
-
-def login_to_gitlab():
-    load_dotenv()
-    gl = gitlab.Gitlab(private_token=os.getenv('BOTTOKEN'), url='https://gitlab.com')
-    get_current_issues(gl)
-
-
-def get_current_issues(gl):
-    project = gl.projects.get(id=33046772)
-    issues = project.issues.list(labels=['Profile Request'], state='opened', order_by='created_at', sort='desc')
-    validate_yaml(issues[0], project)
-    print(issues[0].attributes['description'])
-
-
-def validate_yaml(issue, project):
-    config_schema = Schema({
+    __config_schema = Schema({
         "id": And(str, lambda schema_id: check_for_space(schema_id)),
         "name": str,
         Optional("nick"): And(str, lambda nick: check_for_space(nick)),
@@ -138,30 +135,39 @@ def validate_yaml(issue, project):
             Optional("keybase"): str,
         }
     })
+    __issue_description = ""
+    __issue = None
 
-    try:
-        configuration = yaml.safe_load(issue.attributes['description'])
+    def __init__(self, issue):
+        self.__issue = issue
+        self.__issue_description = issue.attributes['description']
+
+    def validate_yaml(self, gitlab_issue_handler: GitlabIssues):
         try:
-            config_schema.validate(configuration)
+            valid_yaml = yaml.safe_load(self.__issue_description)
+            self.__validate_schema(valid_yaml, gitlab_issue_handler)
+        except ScannerError as error:
+            print(error)
+            message = 'There is a @ symbol in the issues, please remove it.'
+            label = 'Changes Requested'
+            gitlab_issue_handler.print_comment(self.__issue, message, label)
+
+    def __validate_schema(self, yaml_obj, gitlab_issue_handler: GitlabIssues):
+        try:
+            self.__config_schema.validate(yaml_obj)
             print("Configuration is valid.")
-            message = "A merge request has been created for you. Please wait for a moderator to accept your user profile"
+            message = "A merge request has been created for you. Please wait for a moderator to accept your user " \
+                      "profile"
             label = "Request created"
-            print_note(issue, project, message, label)
+            gitlab_issue_handler.print_comment(self.__issue, message, label)
         except SchemaError as se:
             print(se)
             message = str(se)
             label = 'Changes Requested'
-            print_note(issue, project, message, label)
-    except ScannerError as error:
-        print(error)
-        message = 'There is a @ symbol in the issues, please remove it.'
-        label = 'Changes Requested'
-        print_note(issue, project, message, label)
+            gitlab_issue_handler.print_comment(self.__issue, message, label)
 
-def create_merge_request():
-    pass
 
-def check_for_space(tag: str) -> bool:
+def check_for_space( tag: str) -> bool:
     if tag.find(" ") == -1:
         return True
     print("Make sure that '" + tag + "' does not contain any whitespace")
@@ -174,13 +180,95 @@ def check_for_at_symbol(tag: str) -> bool:
     print("Make sure that your usernames does not contain the @ symbol")
     return False
 
+# def login_to_gitlab():
+#     load_dotenv()
+#     gl = gitlab.Gitlab(private_token=os.getenv('BOTTOKEN'), url='https://gitlab.com')
+#     get_current_issues(gl)
 
-def print_note(issue, project, message: str, label: str):
-    issue_t = project.issues.get(issue.attributes['iid'])
-    issue_t.labels = [label]
-    issue_t.save()
-    response = issue_t.notes.create({'body': message})
-    print(response)
+
+# def get_current_issues(gl):
+#     project = gl.projects.get(id=33046772)
+#     issues = project.issues.list(labels=['Profile Request'], state='opened', order_by='created_at', sort='desc')
+#     validate_yaml(issues[0], project)
+#     print(issues[0].attributes['description'])
+
+
+# def validate_yaml(issue, project):
+#     config_schema = Schema({
+#         "id": And(str, lambda schema_id: check_for_space(schema_id)),
+#         "name": str,
+#         Optional("nick"): And(str, lambda nick: check_for_space(nick)),
+#         Optional("pronoun"): str,
+#         Optional("bio"): str,
+#         Optional("country"): str,
+#         "avatar": Or(str, bool),
+#         Optional("position"): str,
+#         "badges": Or(str, [
+#             str
+#         ]),
+#         "active": bool,
+#         Optional("openToWork"): bool,
+#         "projects": Or(str, [
+#             str
+#         ]),
+#         Optional("events"): Or(str, [
+#             str
+#         ]),
+#         Optional("mentor"): Or(str, [
+#             str
+#         ]),
+#         Optional("mentee"): Or(str, [
+#             str
+#         ]),
+#         "social": {
+#             "gitlab": str,
+#             Optional("blog"): str,
+#             Optional("twitter"): str,
+#             Optional("matrix"): str,
+#             Optional("instagram"): str,
+#             Optional("dev_to"): str,
+#             Optional("github"): str,
+#             Optional("mastodon"): str,
+#             Optional("keybase"): str,
+#         }
+#     })
+#
+#
+#
+#     try:
+#         configuration = yaml.safe_load(issue.attributes['description'])
+#         try:
+#             config_schema.validate(configuration)
+#             print("Configuration is valid.")
+#             message = "A merge request has been created for you. Please wait for a moderator to accept your user profile"
+#             label = "Request created"
+#             print_note(issue, project, message, label)
+#         except SchemaError as se:
+#             print(se)
+#             message = str(se)
+#             label = 'Changes Requested'
+#             print_note(issue, project, message, label)
+#     except ScannerError as error:
+#         print(error)
+#         message = 'There is a @ symbol in the issues, please remove it.'
+#         label = 'Changes Requested'
+#         print_note(issue, project, message, label)
+#
+# def create_merge_request():
+#     pass
+#
+#
+#
+#
+#
+#
+#
+# def print_note(issue, project, message: str, label: str):
+#     issue_t = project.issues.get(issue.attributes['iid'])
+#     issue_t.labels = [label]
+#     issue_t.save()
+#     response = issue_t.notes.create({'body': message})
+#     print(response)
 
 
 if __name__ == '__main__':
